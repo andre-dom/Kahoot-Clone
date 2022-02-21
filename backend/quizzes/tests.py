758bc08dec6a6bed0e4b8a3e3f754934ebc96e0b
Andre <72768758+andre-dom@users.noreply.github.com>
2022-02-21 12:53:45 -0800

    start quiz tests

--- backend/quizzes/tests.py
+++ backend/quizzes/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from django.contrib.auth.models import User
+
+class QuizPostTests(APITestCase):
+	def setUp(self):
+		self.user1 = User.objects.create(username='user1')
+		self.user1.set_password("password")
+		self.user1.save()
+
+	def test_post_quiz_with_no_questions(self):
+		"""
+		If a quiz has no questions, fail to validate
+		"""
+		url = reverse("quiz-list")
+		data = {
+			'name': 'a quiz with no questions',
+			'questions': []
+		}
+		self.client.force_authenticate(user=self.user1)
+		response = self.client.post(url, data, format='json')
+		self.client.force_authenticate(user=None)
+		self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
