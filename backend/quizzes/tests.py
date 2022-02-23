9d363d536368dd46ed25226de1be8b07cec4739e
Thomas Lao <thomas.lao@sjsu.edu>
2022-02-22 21:48:22 -0800

    Created 4 tests for quiz answers

--- backend/quizzes/tests.py
+++ backend/quizzes/tests.py
@@ -3,23 +3,75 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 from django.contrib.auth.models import User
+from .models import Quiz
+
 
 class QuizPostTests(APITestCase):
-	def setUp(self):
-		self.user1 = User.objects.create(username='user1')
-		self.user1.set_password("password")
-		self.user1.save()
+    def setUp(self):
+        self.user1 = User.objects.create(username='user1')
+        self.user1.set_password("password")
+        self.user1.save()
 
-	def test_post_quiz_with_no_questions(self):
-		"""
-		If a quiz has no questions, fail to validate
-		"""
-		url = reverse("quiz-list")
-		data = {
-			'name': 'a quiz with no questions',
-			'questions': []
-		}
-		self.client.force_authenticate(user=self.user1)
-		response = self.client.post(url, data, format='json')
-		self.client.force_authenticate(user=None)
-		self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    def test_post_quiz_with_no_questions(self):
+        """
+        If a quiz has no questions, fail to validate
+        """
+        url = reverse("quiz-list")
+        data = {
+            'name': 'a quiz with no questions',
+            'questions': []
+        }
+        self.client.force_authenticate(user=self.user1)
+        response = self.client.post(url, data, format='json')
+        self.client.force_authenticate(user=None)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_post_quiz_with_no_answers(self):
+        """
+        If a quiz has no questions, fail to validate
+        """
+        url = reverse("quiz-list")
+
+        data = {
+            'name': 'a quiz with no answers',
+
+            'questions': [{'answers': []}]
+        }
+        self.client.force_authenticate(user=self.user1)
+        response = self.client.post(url, data, format='json')
+        self.client.force_authenticate(user=None)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_post_quiz_with_more_than_4_answers(self):
+        """
+        If a quiz has no questions, fail to validate
+        """
+        url = reverse("quiz-list")
+
+        data = {
+            'name': 'a quiz with no answers',
+
+            'questions': [{'answers': [{'a': 'b', 'b': 'c', 'd': 'e', 'f': 'g', 'h': 'i'}]}]
+        }
+        self.client.force_authenticate(user=self.user1)
+        response = self.client.post(url, data, format='json')
+        self.client.force_authenticate(user=None)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_post_quiz_with_4_answers(self):
+        """
+        If a quiz has no questions, fail to validate
+        """
+        url = reverse("quiz-list")
+
+        data = {
+            'name': 'a quiz with no answers',
+
+            'questions': [{'answers': [{'a': 'b', 'b': 'c', 'd': 'e', 'f': 'g'}]}]
+        }
+        self.client.force_authenticate(user=self.user1)
+        response = self.client.post(url, data, format='json')
+        self.client.force_authenticate(user=None)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
