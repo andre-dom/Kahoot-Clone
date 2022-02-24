9255f5c8b3257dd51644cef690a296bb2c4e7cb2
Thomas Lao <thomas.lao@sjsu.edu>
2022-02-24 13:49:24 -0800

    fixed my test cases

--- backend/quizzes/tests.py
+++ backend/quizzes/tests.py
@@ -27,52 +27,112 @@ class QuizPostTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data, {"non_field_errors": ["Must be at least one question"]})
 
-    def test_post_quiz_with_no_answers(self):
-        """
-        If a quiz has no questions, fail to validate
-        """
-        url = reverse("quiz-list")
-
-        data = {
-            'name': 'a quiz with no answers',
-
-            'questions': [{'answers': []}]
-        }
-        self.client.force_authenticate(user=self.user1)
-        response = self.client.post(url, data, format='json')
-        self.client.force_authenticate(user=None)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-    def test_post_quiz_with_more_than_4_answers(self):
-        """
-        If a quiz has no questions, fail to validate
-        """
-        url = reverse("quiz-list")
-
-        data = {
-            'name': 'a quiz with no answers',
-
-            'questions': [{'answers': [{'a': 'b', 'b': 'c', 'd': 'e', 'f': 'g', 'h': 'i'}]}]
-        }
-        self.client.force_authenticate(user=self.user1)
-        response = self.client.post(url, data, format='json')
-        self.client.force_authenticate(user=None)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
     def test_post_quiz_with_4_answers(self):
         """
-        If a quiz has no questions, fail to validate
+        If a quiz has a question with 4 answers, success validation
         """
         url = reverse("quiz-list")
-
         data = {
-            'name': 'a quiz with no answers',
+            "name": "string",
+            "questions": [
+                {
+                    "question_body": "string",
+                    "answers": [
+                        {
+                            "answer_body": "a",
 
-            'questions': [{'answers': [{'a': 'b', 'b': 'c', 'd': 'e', 'f': 'g'}]}]
+                        },
+                        {
+                            "answer_body": "b",
+
+                        },
+                        {
+                            "answer_body": "c",
+
+                        },
+                        {
+                            "answer_body": "d"
+                        }
+                    ],
+                    "correct_answer": 4
+                }
+            ]
+        }
+        self.client.force_authenticate(user=self.user1)
+        response = self.client.post(url, data, format='json')
+        self.client.force_authenticate(user=None)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_post_quiz_with_3_answers(self):
+        """
+        If a quiz has a question with 3 answers, fail  to validate
+        """
+        url = reverse("quiz-list")
+        data = {
+            "name": "string",
+            "questions": [
+                {
+                    "question_body": "string",
+                    "answers": [
+                        {
+                            "answer_body": "a",
+
+                        },
+                        {
+                            "answer_body": "b",
+
+                        },
+                        {
+                            "answer_body": "c",
+
+                        }
+
+                    ],
+                    "correct_answer": 4
+                }
+            ]
         }
         self.client.force_authenticate(user=self.user1)
         response = self.client.post(url, data, format='json')
         self.client.force_authenticate(user=None)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
+    def test_post_quiz_with_5_answers(self):
+        """
+        If a quiz has a question with 5 answers, fail to validate
+        """
 
+        url = reverse("quiz-list")
+        data = {
+            "name": "string",
+            "questions": [
+                {
+                    "question_body": "string",
+                    "answers": [
+                        {
+                            "answer_body": "a",
+
+                        },
+                        {
+                            "answer_body": "b",
+
+                        },
+                        {
+                            "answer_body": "c",
+
+                        },
+                        {
+                            "answer_body": "d"
+                        },
+                        {
+                            "answer_body": "e"
+                        }
+                    ],
+                    "correct_answer": 4
+                }
+            ]
+        }
+        self.client.force_authenticate(user=self.user1)
+        response = self.client.post(url, data, format='json')
+        self.client.force_authenticate(user=None)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
