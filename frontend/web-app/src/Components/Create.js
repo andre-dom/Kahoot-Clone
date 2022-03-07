43498e6d70c65f8542269035af8daafe25b1da5b
CarrieyqZhang <yanqi.zhang@sjsu.edu>
2022-03-06 23:40:46 -0800

    fetch API functions updated
    updated API fetch functions in KahootProvider.js and create.js test file

--- frontend/web-app/src/Components/Create.js
+++ frontend/web-app/src/Components/Create.js
@@ -1,4 +1,4 @@
-import { Button } from "@chakra-ui/react";
+import { Input, Button } from "@chakra-ui/react";
 import { useState } from "react";
 import useAuth from "../hooks/useAuth";
 
@@ -6,35 +6,37 @@ const Create = () => {
 
 
   const { auth } = useAuth(); 
+  const[quizzes, setQuizzes] = useState([]);
+  const[name, setName] = useState("");
+  const[question, setQuestion] = useState("");
+  const[answer1, setAnswer1] = useState("");
+  const[answer2, setAnswer2] = useState("");
+  const[answer3, setAnswer3] = useState("");
+  const[answer4, setAnswer4] = useState("");
+  const[correctAnswer, setCorrectAnswer] = useState("");
 
-  //const [quizzes, setQuizzes] = useState([]);
   const myData = 
     {
-      name: "testing",
+      name: `${name}`,
       questions: [
         {
-          question_body: "question_body",
+          question_body: `${question}`,
           answers: [
             {
-                answer_body: "stuff"
+                answer_body: `${answer1}`
+                            },
+            {
+                answer_body: `${answer2}`
                 
             },
             {
-                answer_body: "stuff1"
-                
+                answer_body: `${answer3} `             
             },
             {
-                answer_body: "stuff2"
-                
-            },
-            {
-                answer_body: "stuff3"
-                
-            }
-            
-            
+                answer_body: `${answer4}`            
+            }           
           ],
-          correct_answer: 1
+          correct_answer: `${correctAnswer}`
         }
 
       ]
@@ -42,7 +44,7 @@ const Create = () => {
     }
 
   const postData = async () => {
-
+   
     const response = await fetch('http://127.0.0.1:8000/quizzes/', {
       method: 'POST',
       headers: {
@@ -51,7 +53,6 @@ const Create = () => {
       },
       body: JSON.stringify(myData)
     }); 
-
     if(!response.ok) {
 
       console.log(response.text()); 
@@ -67,17 +68,108 @@ const Create = () => {
 
   const getData = async () => {
 
+      const response = await fetch('http://127.0.0.1:8000/quizzes/', {
+      method: 'Get',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `token ${auth.token}`
+      },
+      //body: JSON.stringify(myData)
+    }); 
+
+    if(!response.ok) {
+
+      console.log(response.text()); 
+
+      console.log('there was an error.');
+      return; 
+    }
+
+    const result = await response.json(); 
+    
+    setQuizzes(result);
+   
+   
+    
+
 
 
   }; 
+  const deleteData = async () => {
+const response = await fetch('http://127.0.0.1:8000/quizzes/hello/', {
+      method: 'Delete',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `token ${auth.token}`
+      },
+      //body: JSON.stringify(myData)
+    }); 
+
+    if(!response.ok) {
+
+      console.log(response.text()); 
+
+      console.log('there was an error.');
+      return; 
+    }
+
+
+  }; 
+   const updateData = async () => {
+     console.warn('data', myData)
+    const response = await fetch('http://127.0.0.1:8000/quizzes/hello/', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `token ${auth.token}`
+      },
+      body: JSON.stringify(myData)
+    }); 
+    if(!response.ok) {
+
+      console.log(response.text()); 
+
+      console.log('there was an error.');
+      return; 
+    }
+
+    const result = await response.json(); 
+
+    console.log(result); 
+  }
 
   return (
     <div className="create">
-      <Button onClick = {postData}>Post Data</Button>
-      <br></br>
-      <br></br>
-      <br></br>
+      <h1> QUIZ</h1>
+    <Input  size="md" placeholder='Name'  type="text" name="name" value={name} onChange = {(e)=>setName(e.target.value)}/>
+      <Input  size="md" placeholder='Question'  type="text" name="question" value={question} onChange = {(e)=>setQuestion(e.target.value)}/>
+        <Input variant='filled' htmlSize={20} width='auto' placeholder='answer 1' type="text" name="answer1" value={answer1} onChange = {(e)=>setAnswer1(e.target.value)}/>
+        <Input variant='filled' htmlSize={20} width='auto' placeholder='answer 2' type="text" name="answer2" value={answer2} onChange = {(e)=>setAnswer2(e.target.value)}/>
+        <Input variant='filled' htmlSize={20}width='auto' placeholder='answer 3' type="text" name="answer3" value={answer3} onChange = {(e)=>setAnswer3(e.target.value)}/>
+        <Input variant='filled' htmlSize={20}width='auto' placeholder='answer 4' type="text" name="answer3" value={answer4} onChange = {(e)=>setAnswer4(e.target.value)}/>
+         <Input variant='filled' htmlSize={20}width='auto' placeholder='correct answer' type="text" name="correctAnswer" value={correctAnswer} onChange = {(e)=>setCorrectAnswer(e.target.value)}/>
+         <br></br>
+      <Button onClick = {postData}>Post Quiz</Button>
+      <Button onClick = {updateData}>Update Quiz</Button>
+      <Button onClick = {deleteData}>Delete Data</Button>
       <Button onClick = {getData}>Get Data</Button>
+      {
+        quizzes.map((quiz, i) => 
+          <ul key={i}>
+            <li>Slug: {quiz.slug}</li>
+            <li>Name: {quiz.name}</li>
+            <li>Question: {quiz.questions[0].question_body}</li>
+            <li>Answer 1: {quiz.questions[0].answers[0].answer_body}</li>
+            <li>Answer 2: {quiz.questions[0].answers[1].answer_body}</li>
+            <li>Answer 3: {quiz.questions[0].answers[2].answer_body}</li>
+            <li>Answer 4: {quiz.questions[0].answers[3].answer_body}</li>
+            <li>Correct Answer: {quiz.questions[0].correct_answer}</li>
+            <br/>           
+      </ul>)
+      } 
+
+      
+
       
     </div>
   );
--- frontend/web-app/src/Context/Kahoot/KahootProvider.js
+++ frontend/web-app/src/Context/Kahoot/KahootProvider.js
@@ -1,4 +1,5 @@
 import { createContext, useState } from 'react'; 
+import useAuth from "../../hooks/useAuth";
 
 const KahootContext = createContext({}); 
 
@@ -6,13 +7,142 @@ export const KahootProvider = ({ children }) => {
 
   //* You can write you functions that call to the API here! Remember that you are going to need state */
 
+const { auth } = useAuth(); 
+  const[quizzes, setQuizzes] = useState([]);
+  const[name, setName] = useState("");
+  const[question, setQuestion] = useState("");
+  const[answer1, setAnswer1] = useState("");
+  const[answer2, setAnswer2] = useState("");
+  const[answer3, setAnswer3] = useState("");
+  const[answer4, setAnswer4] = useState("");
+  const[correctAnswer, setCorrectAnswer] = useState("");
+
+  const myData = 
+    {
+      name: `${name}`,
+      questions: [
+        {
+          question_body: `${question}`,
+          answers: [
+            {
+                answer_body: `${answer1}`
+                            },
+            {
+                answer_body: `${answer2}`
+                
+            },
+            {
+                answer_body: `${answer3} `             
+            },
+            {
+                answer_body: `${answer4}`            
+            }           
+          ],
+          correct_answer: `${correctAnswer}`
+        }
+
+      ]
+    
+    }
+
+  const postData = async () => {
+   
+    const response = await fetch('http://127.0.0.1:8000/quizzes/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `token ${auth.token}`
+      },
+      body: JSON.stringify(myData)
+    }); 
+    if(!response.ok) {
+
+      console.log(response.text()); 
+
+      console.log('there was an error.');
+      return; 
+    }
+
+    const result = await response.json(); 
+
+    console.log(result); 
+  }
+
+  const getData = async () => {
+
+      const response = await fetch('http://127.0.0.1:8000/quizzes/', {
+      method: 'Get',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `token ${auth.token}`
+      },
+      //body: JSON.stringify(myData)
+    }); 
+
+    if(!response.ok) {
+
+      console.log(response.text()); 
+
+      console.log('there was an error.');
+      return; 
+    }
+
+    const result = await response.json(); 
+    
+    setQuizzes(result);
+  
+
+  }; 
+  const deleteData = async () => {
+const response = await fetch('http://127.0.0.1:8000/quizzes/hello/', {
+      method: 'Delete',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `token ${auth.token}`
+      },
+      //body: JSON.stringify(myData)
+    }); 
+
+    if(!response.ok) {
+
+      console.log(response.text()); 
+
+      console.log('there was an error.');
+      return; 
+    }
 
 
+  }; 
+   const updateData = async () => {
+     console.warn('data', myData)
+    const response = await fetch('http://127.0.0.1:8000/quizzes/hello/', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `token ${auth.token}`
+      },
+      body: JSON.stringify(myData)
+    }); 
+    if(!response.ok) {
 
+      console.log(response.text()); 
+
+      console.log('there was an error.');
+      return; 
+    }
+
+    const result = await response.json(); 
+
+    console.log(result); 
+  }
 
 
   //* You can place the functions and state in this object once you are done writing them */
   const values = {
+    postData,
+    getData,
+    deleteData,
+    updateData
 
 
   }; 
